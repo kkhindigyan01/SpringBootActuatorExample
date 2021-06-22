4822d67dda87b3a345e7daf18367503f04b57f8e
kkhindigyan01 <kkhindigyan01@gmail.com>
2021-06-22 17:21:24 +0530

    first commit

---
+++ src/main/java/com/kkhindigyan/app/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.kkhindigyan.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping(path = "/welcome")
+	public String welcome() {
+		return "Hello World!";
+	}
+
+}
